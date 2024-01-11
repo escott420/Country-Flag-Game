9df3fc03e72274527ecc6712881c07180dd7056e
Ethan <ethan.scott2@icloud.com>
2024-01-10 19:26:23 -0600

    Modified AnswerRow

--- Country Flag Game/Views/ContentView.swift
+++ Country Flag Game/Views/ContentView.swift
@@ -11,29 +11,29 @@ struct ContentView: View {
     @StateObject var quizManager = QuizManager()
     var body: some View {
         NavigationView {
-             VStack (spacing: 40) {
-                 VStack(spacing: 20) {
-                     Text ("Country Flag Game")
-                         .font (.title)
-                         .fontWeight (.heavy)
-                         .foregroundColor(.yellow)
-                 }
-        Text ("Ready to test your skillz?")
-                       .foregroundColor (.yellow)
-        }
-            NavigationLink {
-                QuestionView()
-                    .environmentObject (quizManager)
-            } label: {
-                CustomButton (text: "Start")
+            VStack (spacing: 40) {
+                VStack(spacing: 20) {
+                    Text ("Country Flag Game")
+                        .font (.title)
+                        .fontWeight (.heavy)
+                        .foregroundColor(.yellow)
+                    Text ("Ready to test your skillz?")
+                        .foregroundColor (.yellow)
+                }
+                
+                NavigationLink {
+                    QuestionView()
+                        .environmentObject (quizManager)
+                } label: {
+                    CustomButton(text: "Start")
+                }
             }
+            .frame (maxWidth: .infinity, maxHeight: .infinity)
+            .ignoresSafeArea(.all)
+            .background(.cyan)
         }
-        .frame (maxWidth: .infinity, maxHeight: .infinity)
-        .ignoresSafeArea(.all)
-        .background(.cyan)
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
